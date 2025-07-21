0b1f62c150a85e096695523319e4727600c474ac
heora <yueluo.yang@gmail.com>
2025-07-22 06:38:14 +0800

    fix: use dotenv load environment variable

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 4000);
+
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+
+  console.log(`🚀 Server is running on port ${port}`);
 }
 bootstrap();
